20496a3e524790cce920ae7baa3bd1ca3068685b
Jaime Ferrando <fhjaime96@gmail.com>
2020-07-31 20:36:05 +0200

    First commit for deepspeech

---
+++ solver/solver.py
@@ -0,0 +1,80 @@
+import os
+import torch
+import torch.nn as nn
+import torch.utils.data as data
+import torch.optim as optim
+import torch.nn.functional as F
+import torchaudio
+import numpy as np
+
+
+def train(model, device, train_loader, criterion, optimizer, scheduler, epoch, iter_meter):
+    model.train()
+    data_len = len(train_loader.dataset)
+    for batch_idx, _data in enumerate(train_loader):
+        spectrograms, labels, input_lengths, label_lengths = _data
+        spectrograms, labels = spectrograms.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+
+        output = model(spectrograms)  # (batch, time, n_class)
+        output = F.log_softmax(output, dim=2)
+        output = output.transpose(0, 1)  # (time, batch, n_class)
+
+        loss = criterion(output, labels, input_lengths, label_lengths)
+        loss.backward()
+
+        # experiment.log_metric("loss", loss.item(), step=iter_meter.get())
+        # experiment.log_metric("learning_rate", scheduler.get_lr(), step=iter_meter.get())
+
+        optimizer.step()
+        scheduler.step()
+        iter_meter.step()
+        if batch_idx % 100 == 0 or batch_idx == data_len:
+            print(
+                "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
+                    epoch,
+                    batch_idx * len(spectrograms),
+                    data_len,
+                    100.0 * batch_idx / len(train_loader),
+                    loss.item(),
+                )
+            )
+
+
+def test(model, device, test_loader, criterion, epoch, iter_meter):
+    print("\nevaluating...")
+    model.eval()
+    test_loss = 0
+    test_cer, test_wer = [], []
+    with torch.no_grad():
+        for i, _data in enumerate(test_loader):
+            spectrograms, labels, input_lengths, label_lengths = _data
+            spectrograms, labels = spectrograms.to(device), labels.to(device)
+
+            output = model(spectrograms)  # (batch, time, n_class)
+            output = F.log_softmax(output, dim=2)
+            output = output.transpose(0, 1)  # (time, batch, n_class)
+
+            loss = criterion(output, labels, input_lengths, label_lengths)
+            test_loss += loss.item() / len(test_loader)
+
+            decoded_preds, decoded_targets = GreedyDecoder(
+                output.transpose(0, 1), labels, label_lengths
+            )
+            for j in range(len(decoded_preds)):
+                test_cer.append(cer(decoded_targets[j], decoded_preds[j]))
+                test_wer.append(wer(decoded_targets[j], decoded_preds[j]))
+
+    avg_cer = sum(test_cer) / len(test_cer)
+    avg_wer = sum(test_wer) / len(test_wer)
+    # experiment.log_metric("test_loss", test_loss, step=iter_meter.get())
+    # experiment.log_metric("cer", avg_cer, step=iter_meter.get())
+    # experiment.log_metric("wer", avg_wer, step=iter_meter.get())
+
+    print(
+        "Test set: Average loss: {:.4f}, Average CER: {:4f} Average WER: {:.4f}\n".format(
+            test_loss, avg_cer, avg_wer
+        )
+    )
+
---
+++ train.py
@@ -0,0 +1,144 @@
+import os
+import torch
+import torch.nn as nn
+import torch.utils.data as data
+import torch.optim as optim
+import torch.nn.functional as F
+import torchaudio
+from torch.utils.tensorboard import SummaryWriter
+from model.deepspeech import DeepSpeech
+from utils.functions import *
+from solver.solver import train, test
+import numpy as np
+import random
+import argparse
+import yaml
+
+# Fix seed
+seed = 17
+np.random.seed(seed)
+torch.manual_seed(seed)
+random.seed(seed)
+
+# Args
+parser = argparse.ArgumentParser(description="Training script for DeepSpeech on Librispeech .")
+parser.add_argument(
+    "--config_path",
+    metavar="config_path",
+    type=str,
+    help="Path to config file for training.",
+    required=True,
+)
+parser.add_argument(
+    "--experiment_name",
+    metavar="experiment_name",
+    type=str,
+    help="Name for tensorboard experiment logs",
+    default="",
+)
+
+
+class IterMeter(object):
+    """keeps track of total iterations"""
+
+    def __init__(self):
+        self.val = 0
+
+    def step(self):
+        self.val += 1
+
+    def get(self):
+        return self.val
+
+
+def main(
+    train_url="train-other-500", test_url="test-clean",
+):
+
+    writer = SummaryWriter(comment=args.experiment_name)
+    # Load config file for experiment
+    config_path = args.config_path
+    print("---------------------------------------")
+    print("Loading configure file at", config_path)
+    with open(config_path, "r") as f:
+        hparams = yaml.load(f, Loader=yaml.FullLoader)
+
+    learning_rate = hparams["learning_rate"]
+    batch_size = hparams["batch_size"]
+    print(batch_size)
+    epochs = hparams["epochs"]
+    # experiment.log_parameters(hparams)
+
+    use_cuda = torch.cuda.is_available()
+    torch.manual_seed(7)
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    if not os.path.isdir("./data"):
+        os.makedirs("./data")
+
+    # Load datasets
+    print("---------------------------------------")
+    print("Loading datasets...", flush=True)
+    train_dataset = data.ConcatDataset(
+        [
+            torchaudio.datasets.LIBRISPEECH("./data", url=path, download=True)
+            for path in hparams["libri_train_set"]
+        ]
+    )
+    test_dataset = torchaudio.datasets.LIBRISPEECH(
+        "./data", url=hparams["libri_test_set"], download=True
+    )
+    kwargs = {"num_workers": 1, "pin_memory": True} if use_cuda else {}
+    train_loader = data.DataLoader(
+        dataset=train_dataset,
+        batch_size=hparams["batch_size"],
+        shuffle=True,
+        collate_fn=lambda x: data_processing(x, "train"),
+        **kwargs
+    )
+    test_loader = data.DataLoader(
+        dataset=test_dataset,
+        batch_size=hparams["batch_size"],
+        shuffle=False,
+        collate_fn=lambda x: data_processing(x, "valid"),
+        **kwargs
+    )
+
+    print("---------------------------------------")
+    print("Creating model architecture...", flush=True)
+    model = DeepSpeech(
+        hparams["n_cnn_layers"],
+        hparams["n_rnn_layers"],
+        hparams["rnn_dim"],
+        hparams["n_class"],
+        hparams["n_feats"],
+        hparams["stride"],
+        hparams["dropout"],
+    ).to(device)
+
+    print(model)
+    print("Num Model Parameters", sum([param.nelement() for param in model.parameters()]))
+
+    optimizer = optim.AdamW(model.parameters(), hparams["learning_rate"])
+    criterion = nn.CTCLoss(blank=28).to(device)
+    scheduler = optim.lr_scheduler.OneCycleLR(
+        optimizer,
+        max_lr=hparams["learning_rate"],
+        steps_per_epoch=int(len(train_loader)),
+        epochs=hparams["epochs"],
+        anneal_strategy="linear",
+    )
+
+    print("---------------------------------------")
+    print("Training...", flush=True)
+    iter_meter = IterMeter()
+    for epoch in range(1, epochs + 1):
+        train(
+            model, device, train_loader, criterion, optimizer, scheduler, epoch, iter_meter,
+        )
+        test(model, device, test_loader, criterion, epoch, iter_meter)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    main()
